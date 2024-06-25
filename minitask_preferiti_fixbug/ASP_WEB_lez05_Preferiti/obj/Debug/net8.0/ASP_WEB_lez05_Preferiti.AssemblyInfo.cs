c94a0d0125a8a490be0fa7cd4643a166f07cad10
JhoelCanulli <115298625+JhoelCanulli@users.noreply.github.com>
2024-06-25 23:39:16 +0200

    no update

--- minitask_preferiti_fixbug/ASP_WEB_lez05_Preferiti/obj/Debug/net8.0/ASP_WEB_lez05_Preferiti.AssemblyInfo.cs
+++ minitask_preferiti_fixbug/ASP_WEB_lez05_Preferiti/obj/Debug/net8.0/ASP_WEB_lez05_Preferiti.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASP_WEB_lez05_Preferiti")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+551dcbc0cb8b85506b0099e53323f3bebd4f4be2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d0e4f9b836980452c5eaddec9df21e10970fcef")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASP_WEB_lez05_Preferiti")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASP_WEB_lez05_Preferiti")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
