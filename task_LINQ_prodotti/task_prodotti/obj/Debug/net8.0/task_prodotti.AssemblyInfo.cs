7a604a2dc7d796ee96cf1ad66c6db8ca4c12c729
JhoelCanulli <115298625+JhoelCanulli@users.noreply.github.com>
2024-06-25 23:37:32 +0200

    fix stored procedures

--- task_LINQ_prodotti/task_prodotti/obj/Debug/net8.0/task_prodotti.AssemblyInfo.cs
+++ task_LINQ_prodotti/task_prodotti/obj/Debug/net8.0/task_prodotti.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task_prodotti")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da690f816fec9d5db649c888897b87205e739cc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("task_prodotti")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task_prodotti")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
